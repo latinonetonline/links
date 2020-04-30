35ac8f5820c92d8f6268acfc7467f25aa8195de4
lautaro <lautaroecarro@gmail.com>
2020-04-30 20:04:06 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var hash = location.hash.substr(1);
+fetch(`https://raw.githubusercontent.com/latinonetonline/linksdb/master/${hash}`)
+.then(data => data.json())
+    .then(data => window.location.href = data.url)
+
+
+
