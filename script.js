73ffec05c4e6d73457d5f6b549d93713d1ee3a47
Lautaro Carro <lautaroecarro@gmail.com>
2020-05-01 00:11:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var hash = location.hash.substr(1);
-fetch(`https://raw.githubusercontent.com/latinonetonline/linksdb/master/${hash}`)
+fetch(`https://raw.githubusercontent.com/latinonetonline/linksdb/master/links/${hash}`)
 .then(data => data.json())
     .then(data => window.location.href = data.Url)
 
